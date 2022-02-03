222528ab00d5676c276bf1d9048ecd90d9ae3a30
ahmed maher <ahmedmaher1792001@gmail.com>
2022-02-03 14:09:07 +0200

    some edit in site login view

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -1,115 +1,116 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="loading" lang="en" data-textdirection="ltr">
 <head>
-	<title>Login V5</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{url('public/site/login/images/icons/favicon.ico')}}"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/vendor/bootstrap/css/bootstrap.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/vendor/animate/animate.css')}}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/vendor/css-hamburgers/hamburgers.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/vendor/animsition/css/animsition.min.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/vendor/select2/select2.min.css')}}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/vendor/daterangepicker/daterangepicker.css')}}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/css/util.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{url('public/site/login/css/main.css')}}">
-<!--===============================================================================================-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+  <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+  <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+  <meta name="author" content="PIXINVENT">
+  <title>login
+  </title>
+  <link rel="apple-touch-icon" href="{{url('public/admin/theme/app-assets/images/ico/apple-icon-120.png')}}">
+  <link rel="shortcut icon" type="image/x-icon" href="{{url('public/admin/theme/app-assets/images/ico/favicon.ico')}}">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+  rel="stylesheet">
+  <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
+  rel="stylesheet">
+  <!-- BEGIN VENDOR CSS-->
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/css/vendors.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/vendors/css/forms/icheck/icheck.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/vendors/css/forms/icheck/custom.css')}}">
+  <!-- END VENDOR CSS-->
+  <!-- BEGIN MODERN CSS-->
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/css/app.css')}}">
+  <!-- END MODERN CSS-->
+  <!-- BEGIN Page Level CSS-->
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/css/core/menu/menu-types/vertical-menu-modern.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/css/core/colors/palette-gradient.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/app-assets/css/pages/login-register.css')}}">
+  <!-- END Page Level CSS-->
+  <!-- BEGIN Custom CSS-->
+  <link rel="stylesheet" type="text/css" href="{{url('public/admin/theme/assets/css/style.css')}}">
+  <!-- END Custom CSS-->
 </head>
-<body>
+<body class="vertical-layout vertical-menu-modern 1-column   menu-expanded blank-page blank-page"
+data-open="click" data-menu="vertical-menu-modern" data-col="1-column">
+  <!-- ////////////////////////////////////////////////////////////////////////////-->
+  <div class="app-content content">
+    <div class="content-wrapper">
+      <div class="content-header row">
+      </div>
+      <div class="content-body">
+        <section class="flexbox-container">
+          <div class="col-12 d-flex align-items-center justify-content-center">
+            <div class="col-md-4 col-10 box-shadow-2 p-0">
+              <div class="card border-grey border-lighten-3 m-0">
+                <div class="card-header border-0">
+                  <div class="card-title text-center">
+                    <div class="p-1">
+                      <img src="{{url('public/admin/theme/app-assets/images/logo/logo-dark.png')}}" alt="branding logo">
+                    </div>
+                  </div>
+                  <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2">
+                    <span>user</span>
+                  </h6>
+                </div>
+                <div class="container">
+                  @include('vendors.alerts.success')
+                  @include('vendors.alerts.error')
+                </div>
+                <div class="card-content">
+                  <div class="card-body">
+                    <form class="form-horizontal form-simple" action="" method="post">
+                      <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
-	<div class="limiter">
-		<div class="container-login100" style="background-image: url('{{url("public/site/login/images/bg-01.jpg")}}');">
-			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
-				@include('site.alerts.error')
-				@include('site.alerts.success')
+					  {{-- username --}}
+                      <fieldset class="form-group position-relative has-icon-left mb-0" style="margin-bottom: 1.5rem !important">
+                        <input type="text" class="form-control form-control-lg input-lg" id="user-name" placeholder="Your Username"
+                        name="username" required autocomplete="off">
+                        <div class="form-control-position">
+                          <i class="ft-user"></i>
+                        </div>
+                      </fieldset>
 
-				<form class="login100-form validate-form flex-sb flex-w" action="" method="post">
-          			@csrf
-					<span class="login100-form-title p-b-53">
-						Sign In With
-					</span>
+					  {{-- password --}}
+                      <fieldset class="form-group position-relative has-icon-left">
+                        <input type="password" class="form-control form-control-lg input-lg" id="user-password"
+                        placeholder="Enter Password" name="password" required autocomplete="off">
+                        <div class="form-control-position">
+                          <i class="la la-key"></i>
+                        </div>
+                      </fieldset>
 
-					<a href="#" class="btn-face m-b-20">
-						<i class="fa fa-facebook-official"></i>
-						Facebook
-					</a>
+                      <div class="form-group row">
+                        <div class="col-md-6 col-12 text-center text-md-right"><a href="{{url('signUp')}}" class="card-link">Sign Up</a></div>
+                      </div>
+                      <button type="submit" class="btn btn-info btn-lg btn-block"><i class="ft-unlock"></i>Login</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
 
-					<a href="#" class="btn-google m-b-20">
-						<img src="{{url('public/site/login/images/icons/icon-google.png')}}" alt="GOOGLE">
-						Google
-					</a>
-					
-					<div class="p-t-31 p-b-9">
-						<span class="txt1">
-							Username
-						</span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Username is required">
-						<input class="input100" type="text" name="username" >
-						<span class="focus-input100"></span>
-					</div>
-					
-					<div class="p-t-13 p-b-9">
-						<span class="txt1">
-							Password
-						</span>
-					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="password" >
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="container-login100-form-btn m-t-17">
-						<button class="login100-form-btn">
-							Sign In
-						</button>
-					</div>
-
-					<div class="w-full text-center p-t-55">
-						<span class="txt2">
-							Not a member?
-						</span>
-
-						<a href="{{url('signUp')}}" class="txt2 bo1">
-							Sign up now
-						</a>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	
-
-	<div id="dropDownSelect1"></div>
-	
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/vendor/animsition/js/animsition.min.js')}}"></script>
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/vendor/bootstrap/js/popper.js')}}"></script>
-	<script src="{{url('public/site/login/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/vendor/select2/select2.min.js')}}"></script>
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/vendor/daterangepicker/moment.min.js')}}"></script>
-	<script src="{{url('public/site/login/vendor/daterangepicker/daterangepicker.js')}}"></script>
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/vendor/countdowntime/countdowntime.js')}}"></script>
-<!--===============================================================================================-->
-	<script src="{{url('public/site/login/js/main.js')}}"></script>
-
+  <!-- ////////////////////////////////////////////////////////////////////////////-->
+  <!-- BEGIN VENDOR JS-->
+  <script src="{{url('public/admin/theme/app-assets/vendors/js/vendors.min.js')}}" type="text/javascript"></script>
+  <!-- BEGIN VENDOR JS-->
+  <!-- BEGIN PAGE VENDOR JS-->
+  <script src="{{url('public/admin/theme/app-assets/vendors/js/forms/icheck/icheck.min.js')}}" type="text/javascript"></script>
+  <script src="{{url('public/admin/theme/app-assets/vendors/js/forms/validation/jqBootstrapValidation.js')}}"
+  type="text/javascript"></script>
+  <!-- END PAGE VENDOR JS-->
+  <!-- BEGIN MODERN JS-->
+  <script src="{{url('public/admin/theme/app-assets/js/core/app-menu.js')}}" type="text/javascript"></script>
+  <script src="{{url('public/admin/theme/app-assets/js/core/app.js')}}" type="text/javascript"></script>
+  <!-- END MODERN JS-->
+  <!-- BEGIN PAGE LEVEL JS-->
+  <script src="{{url('public/admin/theme/app-assets/js/scripts/forms/form-login-register.js')}}" type="text/javascript"></script>
+  <!-- END PAGE LEVEL JS-->
 </body>
 </html>
